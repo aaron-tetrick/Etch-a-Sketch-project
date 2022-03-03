5fdc998d292baca1c78a825aecd7428748341153
Aaron Tetrick <atetrick14102@gmail.com>
2022-03-03 01:29:33 -0500

    Switched out slider--Put in prompt instead

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const slider = document.querySelector('#slider');
-const sliderValue = document.querySelector('#slider-value');
+const sliderValue = document.querySelector('#grid-value');
 const btn = document.querySelectorAll('.btn')
 const rainbow = document.getElementById('rainbow');
 const black = document.getElementById('black');
@@ -8,13 +8,7 @@ const grid = document.getElementById('grid');
 
 
 
-
-
-function hover() {
-    console.log("hi")
-}
-
-slider.addEventListener('input', gridSizeValue)
+window.onload = clearGrid;
 rainbow.addEventListener('click', () => changeColor('rainbow'));
 black.addEventListener('click', () => changeColor('black'));
 clear.addEventListener('click', clearGrid);
@@ -22,8 +16,18 @@ clear.addEventListener('click', clearGrid);
 
 
 
-function changeGrid() {
-    
+function changeGrid(size) {
+    clearGrid;
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (i=0; i < (size*size); i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    grid.appendChild(cell);
+
+    }
+
 }
 
 
@@ -31,21 +35,34 @@ function changeGrid() {
 
 
 
-function gridSizeValue() {
-    //console.log(e.target.value, slider.value);
-    sliderValue.innerText = `${slider.value} x ${slider.value}`;
+function gridSizeValue(num) {
+    sliderValue.innerText = `${num} x ${num}`;
 }
 
 
 
 
 function clearGrid() {
-    console.log("Clear");
+    grid.innerHTML = ''
+    let gridSize = prompt("Choose a grid size (MAX: 64)");
+    if (gridSize >= 64) {
+        changeGrid(64);
+    } else if (gridSize <= 0) {
+        changeGrid(1);
+    } else if (gridSize < 64 && gridSize > 0) {
+        changeGrid(gridSize);
+    } else {
+        alert("You must enter a number");
+        let gridSize = prompt("Choose a grid size (MAX: 64)");
+    }
+    gridSizeValue(gridSize);
 }
 
 function changeColor(color) {
     if (color === "black") {
-        console.log("Black");
+        console.log('Black')
+        grid.addEventListener("mouseenter", () => grid.style.backgroundColor = "blue")
+        grid.classList.add('black:hover');
     } else if (color = "rainbow") {
         console.log("Rainbow");
     }
