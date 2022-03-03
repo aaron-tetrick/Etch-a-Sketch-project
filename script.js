2ff479dfec620fd2c253fe67252671801744a855
Aaron Tetrick <atetrick14102@gmail.com>
2022-03-03 02:16:24 -0500

    Completed basic functionality of Etch-a-Sketch

--- script.js
+++ script.js
@@ -47,23 +47,37 @@ function clearGrid() {
     let gridSize = prompt("Choose a grid size (MAX: 64)");
     if (gridSize >= 64) {
         changeGrid(64);
+        gridSizeValue(64);
     } else if (gridSize <= 0) {
         changeGrid(1);
+        gridSizeValue(1);
     } else if (gridSize < 64 && gridSize > 0) {
         changeGrid(gridSize);
+        gridSizeValue(gridSize);
     } else {
         alert("You must enter a number");
         let gridSize = prompt("Choose a grid size (MAX: 64)");
     }
-    gridSizeValue(gridSize);
+    
 }
 
 function changeColor(color) {
     if (color === "black") {
         console.log('Black')
-        grid.addEventListener("mouseenter", () => grid.style.backgroundColor = "blue")
-        grid.classList.add('black:hover');
-    } else if (color = "rainbow") {
+        grid.addEventListener("mouseover", (e) => {
+            if (e.target.classList.value === "cell") {
+            e.target.style.backgroundColor = "black";
+            e.target.style.transition = '0.5s';
+            }
+         })
+        } else if (color = "rainbow") {
         console.log("Rainbow");
+        grid.addEventListener("mouseover", (e) => {
+            if (e.target.classList.value === "cell") {
+            e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()* 256)}, ${Math.floor(Math.random()* 256)}, ${Math.floor(Math.random()* 256)})`;
+            e.target.style.transition = '0.5s';
+        }
+         })
     }
 }
+
